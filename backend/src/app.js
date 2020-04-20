eb85052d357a8a26afb98a9963c88f61d74d78ee
Marcos Túlio Rodrigues <marcostulio06@hotmail.com>
2020-04-20 07:35:41 -0300

    go barber init

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.server = express();
+
+    this.middlewares();
+    this.routes();
+  }
+  middlewares() {
+    // preparando pra receber requisições via json
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
