bec9db02c9e98bf37bc168f467b65bcdccff622e
boydwo <marcos.t@ufv.br>
2020-04-26 17:24:10 -0300

    validaçoes

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,8 +1,19 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
+
 import User from '../models/User';
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validarion Fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -22,8 +33,8 @@ class SessionController {
         name,
         email,
       },
-      token: jwt.sign({ id }, ' f29618255c309de4469993cce24286ea', {
-        expiresIn: '7d',
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
       }),
     });
   }
