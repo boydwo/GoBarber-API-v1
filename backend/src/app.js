6627a59c22ee92718c7e7086f954dd9266aca417
boydwo <marcos.t@ufv.br>
2020-04-30 08:09:21 -0300

    providers

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 import './database';
@@ -14,6 +15,12 @@ class App {
   middlewares() {
     // preparando pra receber requisições via json
     this.server.use(express.json());
+
+    // acessar arquivos estaticos
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
